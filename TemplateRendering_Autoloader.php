b8c969fdb1d33722edea8be55c5e7feaf1480bb3
Alex Regan <alex.regan@artisancolour.com>
2016-08-22 16:21:28 -0700

    added model class & logic

--- TemplateRendering_Autoloader.php
+++ TemplateRendering_Autoloader.php
@@ -1,26 +1,18 @@
 <?php
 
 function TemplateRendering_Autoloader($classname) {
-  //Can't use __DIR__ as it's only in PHP 5.3+
-  $filename = dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.$classname.'.class'.'.php';
+  $filename = __DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.$classname.'.class'.'.php';
   if (is_readable($filename)) {
     require_once $filename;
   }
 }
 
-if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
-  //SPL autoloading was introduced in PHP 5.1.2
-  if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
-    spl_autoload_register('TemplateRendering_Autoloader', true, true);
-  } else {
-    spl_autoload_register('TemplateRendering_Autoloader');
-  }
-} else {
-  /**
-   * Fall back to traditional autoload for old PHP versions
-   * @param string $classname The name of the class to load
-   */
-  function __autoload($classname) {
-    TemplateRendering_Autoloader($classname);
+function Model_Autoloader($classname) {
+  $filename = __DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.$classname.'.class'.'.php';
+  if (is_readable($filename)) {
+    require_once $filename;
   }
 }
+
+spl_autoload_register('TemplateRendering_Autoloader', true);
+spl_autoload_register('Model_Autoloader', true);
---
+++ lib/models/Model.class.php
@@ -0,0 +1,129 @@
+<?php
+
+class Model {
+
+  function Model($props = []) {
+    if (!empty($props) && $this->isAssoc($props)) {
+      $this->setAssoc($props);
+    }
+  }
+
+  public function get($prop) {
+    return $this->$prop;
+  }
+
+  public function set($prop, $val) {
+    $this->$prop = $val;
+    return $this->$prop;
+  }
+
+  public function setAssoc($assoc) {
+    // loop over the associative array
+    foreach ($assoc as $key => $value) {
+      // set the prop on the object
+      $this->set($key, $value);
+      if ( in_array( $key, $this->schema ) && $this->schema[$key] !== 'json' ) {
+        // cast each value in $this->schema
+        settype($this->$key, $this->schema[$key]);
+      } elseif ( in_array( $key, $this->schema ) && $this->schema[$key] === 'json' ) {
+        // special case to parse json
+        json_decode($this->$key, true);
+      } // end conditionals
+    } // end foreach
+  } // end setAssoc
+
+  public function isAssoc($arr) {
+    return array_keys($arr) !== range(0, count($arr) - 1);
+  }
+
+  public function parseDate($dateString, $format = '') {
+    if (!empty($format)) {
+      return date($format, strtotime($dateString));
+    } else {
+      return strtotime($dateString);
+    }
+  }
+
+  public function render($template) {
+    $searchStrings = []; $replacements = [];
+    foreach ($this as $key => $value) {
+      array_push($searchStrings, "{{{$key}}}");
+      if (is_array($value)) {
+        $arrayConversion = implode(', ', $value);
+        array_push($replacements, $arrayConversion);
+      } else {
+        array_push($replacements, $value);
+      }
+    }
+    return str_replace($searchStrings, $replacements, $template);
+  }
+
+  # ['prop' => 'type']
+  var $schema = [
+    // BOOLEANS
+    'deleted'           => 'boolean',
+    'used'              => 'boolean',
+    'deleted'           => 'boolean',
+    'notified'          => 'boolean',
+    'canInvite'         => 'boolean',
+    'published'         => 'boolean',
+    'reviewed'          => 'boolean',
+    'matureContent'     => 'boolean',
+    'isApproved'        => 'boolean',
+    'tosAccepted'       => 'boolean',
+    'hasOnboarded'      => 'boolean',
+    'resolutionIsValid' => 'boolean',
+    // INTEGERS
+    'id'                 => 'integer',
+    'fileID'             => 'integer',
+    'productID'          => 'integer',
+    'productClassID'     => 'integer',
+    'sellerID'           => 'integer',
+    'orderID'            => 'integer',
+    'jobID'              => 'integer',
+    'qty'                => 'integer',
+    'invitedBy'          => 'integer',
+    'totalItems'         => 'integer',
+    'sqFeet'             => 'integer',
+    'linFeet'            => 'integer',
+    'resolution'         => 'integer',
+    'sortOrder'          => 'integer',
+    'permissionsLevel'   => 'integer',
+    'reviewReqTimestamp' => 'integer',
+    'favorites'          => 'integer',
+    'ac_reviews'         => 'integer',
+    'ac_score'           => 'integer',
+    'ac_mature_content'  => 'integer',
+    'lastOffset'         => 'integer',
+    'count'              => 'integer',
+    // FLOATS
+    'price'           => 'float',
+    'totalPrice'      => 'float',
+    'totalRetail'     => 'float',
+    'totalCommission' => 'float',
+    'unitPrice'       => 'float',
+    'unitRetail'      => 'float',
+    'unitCommission'  => 'float',
+    'price'           => 'float',
+    'salePrice'       => 'float',
+    'priceTotal'      => 'float',
+    'retailPrice'     => 'float',
+    'commission'      => 'float',
+    'largestDim'      => 'float',
+    // JSON objects
+    'dims'            => 'json',
+    'metaData'        => 'json',
+    'finishing'       => 'json',
+    'moods'           => 'json',
+    'tags'            => 'json',
+    'categories'      => 'json',
+    'reviewedFileIDs' => 'json',
+    // DATE FORMAT
+    'created' => 'date',
+    'modified' => 'date',
+    'placedOn' => 'date',
+  ];
+
+  var $dateFormat = 'Y-m-d H:i:s';
+
+}
